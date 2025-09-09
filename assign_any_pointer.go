a200ad01cd90dfa7f585a9901b688f384c98813e
MWhatsUp <44034470+MWhatsUp@users.noreply.github.com>
2025-09-09 16:50:58 -0300

    Update assign_any_pointer.go

--- assign_any_pointer.go
+++ assign_any_pointer.go
@@ -11,11 +11,11 @@ func main() {
 	*(ptr.(*int)) = 2
 	fmt.Printf("value: %v\n", a)
 
-	variable, ok := ptr.(*string)
+	variable, ok := ptr.(*int)
 	if !ok || variable == nil {
 		_, file, line, _ := runtime.Caller(0)
 		panic(fmt.Sprintf("Error: Incorrect pointer type was used in query parameter handler (%s:%v)", file, line))
 	}
-	*variable = "asInt"
+	*variable = 3
 	fmt.Printf("value: %v\n", a)
 }
